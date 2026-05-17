d6e0562341c0324536b60700634068cd3495b103
AldoCarbajal-3804 <aldocarbajal62@gmail.com>
2026-05-16 22:39:03 -0500

    Configuracion inicial y adicion de elementos

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className='text-red-100 text-7xl'>Ly Pharma</h1>
+      <p>Aplicación de escritorio</p>
+    </div>
+  );
+}
+
+export default App;
