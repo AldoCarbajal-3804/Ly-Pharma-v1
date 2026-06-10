f9300d954ce64005e5a84174f28af2df7c8b396c
AldoCarbajal-3804 <aldocarbajal62@gmail.com>
2026-06-09 22:57:15 -0500

    Rutas invalidas

---
+++ src/routes/NotFound.jsx
@@ -0,0 +1,11 @@
+
+function NotFound(){
+    return(
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <h1>404 - Página no encontrada</h1>
+            <p>Lo sentimos, la página que buscas no existe.</p>
+        </div>
+    )
+}
+
+export default NotFound;
