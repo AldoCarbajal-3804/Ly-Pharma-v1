ed6fb1b83fb8f2f655c30615b8c69abf43cb4f1e
AldoCarbajal-3804 <aldocarbajal62@gmail.com>
2026-06-16 17:12:48 -0500

    Corrigiendo iconos

--- src/views/products/AddProduct.jsx
+++ src/views/products/AddProduct.jsx
@@ -3,6 +3,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useAuth } from "../../hooks/useAuth";
 import { addProduct, editProduct } from "../../services/productService";
 import { getCategoria, getTipos, getProveedores } from "../../services/categoriaServices";
+import close from "../../assets/icons/productos/close.svg"
 
 const capitalize = (s) => s.charAt(0).toUpperCase() + s.slice(1);
 
@@ -104,9 +105,7 @@ export const AddProduct = ({ product, onClose }) => {
                         onClick={onClose}
                         className="p-2 rounded-xl hover:bg-gray-100 text-gray-400 hover:text-gray-600 transition-colors cursor-pointer"
                     >
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
+                        <img src={close} alt="Cerrar" />
                     </button>
                 </div>
 
