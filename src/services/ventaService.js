0ea2010bb6abebc9e59ee3d17d5e3f1c004443a7
AldoCarbajal-3804 <aldocarbajal62@gmail.com>
2026-06-16 17:13:37 -0500

    Implementacion de Servicios de ventas

---
+++ src/services/ventaService.js
@@ -0,0 +1,49 @@
+import { URL } from "../variables";
+const API_URL = `${URL}/ventas`;
+
+export async function getVentas(token) {
+    
+    const res = await fetch(API_URL, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    })
+
+    const data = await res.json()
+    if (!res.ok) throw new Error(data.message || 'Error en el envio de token');
+    return data;
+
+}
+
+export async function getVentasId(token,id) {
+    
+    const res = await fetch(`${API_URL}${id}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    })
+
+    const data = await res.json()
+    if (!res.ok) throw new Error(data.message || 'Error en el envio de token');
+    return data;
+}
+
+export async function addVentas(token,venta) {
+
+    const res = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(venta)
+    });
+    const data = await res.json();
+    if (!res.ok) throw new Error(data.message || 'Error al crear la venta');
+    return data;
+    
+}
