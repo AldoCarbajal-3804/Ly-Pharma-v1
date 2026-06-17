b72f025f8aa2913ff97190211ca79876dc9b922e
AldoCarbajal-3804 <aldocarbajal62@gmail.com>
2026-06-16 20:10:56 -0500

    metodos de recuperacion y reestablecimiento de contraseñas

--- src/services/perfilService.js
+++ src/services/perfilService.js
@@ -1,6 +1,6 @@
 
 import { URL } from "../variables";
-const API_URL = `${URL}/perfil/`
+const API_URL = `${URL}/perfil`
 
 
 export async function getProfile(token) {
@@ -14,6 +14,32 @@ export async function getProfile(token) {
 
     const data = await res.json()
     if (!res.ok) throw new Error(data.message || 'Error en el envio de token');
+    return data;
+}
+
+export async function unforgetPassword(username) {
+    const res = await fetch(`${API_URL}/olvide-password`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(username)
+    });
+    const data = await res.json();
+    if (!res.ok) throw new Error(data.message || 'Error al recibir el token de restablecimiento');
+    return data;
+}
+
+export async function restablecePassowrd(rest) {
+    const res = await fetch(`${API_URL}/restablecer-password`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(rest)
+    });
+    const data = await res.json();
+    if (!res.ok) throw new Error(data.message || 'Error al reestablecer el password');
     return data;
 }
 
