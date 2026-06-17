79f5520f62241535d312a9e123d8e7fcfb25c440
AldoCarbajal-3804 <aldocarbajal62@gmail.com>
2026-06-17 10:52:21 -0500

    Servicios de sesion

---
+++ src/services/sesionesServices.js
@@ -0,0 +1,16 @@
+import { URL } from "../variables";
+const API_URL = `${URL}/sesiones`;
+
+export async function getSesiones(token) {
+    const res = await fetch(API_URL,{
+        method: 'GET',
+        headers: { 
+            'Content-Type': 'application/json' , 
+            'Authorization': `Bearer ${token}`
+        }
+    })
+
+    const data = await res.json()
+    if (!res.ok) throw new Error(data.message || 'Error en el envio de token');
+    return data;
+}
