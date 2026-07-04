4e724c56fefa6e37ae8ab581d499a6db4f4f6381
AldoCarbajal-3804 <aldocarbajal62@gmail.com>
2026-07-04 12:23:05 -0500

    Arreglo de link

--- src/variables.js
+++ src/variables.js
@@ -1,3 +1,2 @@
-const DEV_API = import.meta.env.DEV ? '' : null;
-const API_URL = import.meta.env.VITE_RUTA_API || DEV_API || 'https://ly-pharma-backend.vercel.app';
+const API_URL = import.meta.env.DEV ? '' : (import.meta.env.VITE_RUTA_API || 'https://ly-pharma-backend.vercel.app');
 export const URL = `${API_URL}/api`
